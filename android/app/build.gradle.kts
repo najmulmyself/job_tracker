521dd3b6c6f2c4d047a8ff928919a57af5958e31
najmulmyself <najmul.myself@gmail.com>
2026-01-07 23:03:18 +0600

    feat: Add Firebase Analytics dependency to build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -61,4 +61,6 @@ flutter {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
